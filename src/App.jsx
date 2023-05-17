a50798a5ce0a52d5c32b76dbf9e8cbb853debfa0
jungo <81670100+jungo0@users.noreply.github.com>
2023-05-17 13:35:48 +0900

    products path수정

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
               }
             />
             <Route
-              path="/products"
+              path="/products/list"
               element={
                 <ProductListPage
                   bookmarkState={bookmarkState}
