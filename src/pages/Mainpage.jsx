6a91a1d7da5ec264b3c9c0325bdd3ca7a38db558
jungo <81670100+jungo0@users.noreply.github.com>
2023-05-18 12:38:02 +0900

    토스트 구현

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,13 +1,24 @@
 import BookmarkList from "../components/BookmarkList";
 import ProductList from "../components/Productlist";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Mainpage() {
   const bookmark = JSON.parse(localStorage.getItem("bookmark"));
   const [bookmarkState, setBookmarkState] = useState(bookmark);
+  const [showToast, setShowToast] = useState(true);
 
   return (
     <div>
+        <div>
+        <ToastContainer
+          position="bottom-right"
+          limit={3}
+          closeButton={true}
+          autoClose={3000}
+        />
+        </div>
       <ProductList
         bookmarkState={bookmarkState}
         setBookmarkState={setBookmarkState}
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -3,6 +3,8 @@ import Categories from "../components/Categories";
 import styles from "./ProductListPage.module.css";
 import { useEffect, useState, useRef } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function ProductListPage({ bookmarkState, setBookmarkState }) {
   const [data, setData] = useState([]);
@@ -76,6 +78,12 @@ function ProductListPage({ bookmarkState, setBookmarkState }) {
   };
   return (
     <div className={styles.mainbox}>
+      <ToastContainer
+        position="bottom-right"
+        limit={3}
+        closeButton={true}
+        autoClose={3000}
+      />
       <Categories currentType={currentType} setCurrentType={setCurrentType} />
       <div className={styles.itemBox}>
         {showData
